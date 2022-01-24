844516ba6c5c263cc6e1fe9f3c901c9e04a58031
JM <jnmrles@gmail.com>
2022-01-24 12:10:37 -0600

    giphy api, transaction card

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -2,6 +2,79 @@ import React, {useContext} from "react"
 import {TransactionContext} from "../context/TransactionContext"
 import dummyData from '../utils/dummyData'
 import { shortenAddress } from "../utils/shortenAddress"
+import useFetch from "../hooks/useFetch"
+
+const TransactionCard = ({addressTo, addressFrom, timestamp,message, keyword, amount, url })=>{
+
+  const gifUrl = useFetch({keyword})
+
+  const { connectedAccount } = useContext(TransactionContext)
+ return(
+  <div className="bg-[#181918] m-4 flex flex-1
+  2xl:min-2-[450px]
+  2xl:max-2-[500px]
+  sm:min-w-[270px]
+  sm:max-w-[300px]
+  flex-col p-3 rounded-md hover:shadow-2xl
+
+  ">
+    <div className="flex flex-col items-center w-full md-3">
+      <div className="w-full mb-6 p-2">
+        <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noopener norefferer">
+
+          <p className="text-white text-base">
+            From: {connectedAccount? shortenAddress(addressFrom):"Connect Account"}
+          </p>
+
+        </a>
+
+        <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noopener norefferer">
+
+<p className="text-white text-base">
+  TO: {connectedAccount? shortenAddress(addressTo):"Connect Account"}
+</p>
+
+</a>
+      <p className="text-white text-base">
+        Amount: {amount} ETH
+
+      </p>
+      {message && (
+        <>
+        <br />
+        <p className="text-white text-base">
+          Message: {message}
+
+        </p>
+
+        </>
+      )}
+
+
+      </div>
+      <img src={gifUrl || url} alt="gif" className="w-full h-64 2x:h-96 rounded-md shadow-lg object-cover"/>
+
+<div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+  <p className="text-[#37c7da] font-bold">
+    {timestamp}
+
+  </p>
+
+</div>
+
+    </div>
+
+  </div>
+
+ )
+
+
+}
+
+
+
+
+
 
 const Transactions = () =>{
 
