87585e2a34a4ecbe009aec620e7453a536ef9343
JM <jnmrles@gmail.com>
2022-01-24 10:44:05 -0600

    transaction section

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,8 +1,8 @@
 import React, {useContext} from "react"
-
 import {TransactionContext} from "../context/TransactionContext"
-
 import dummyData from '../utils/dummyData'
+import { shortenAddress } from "../utils/shortenAddress"
+
 const Transactions = () =>{
 
   const { connectedAccount } = useContext(TransactionContext)
@@ -11,8 +11,13 @@ const Transactions = () =>{
   <div className="flex flex-col md:p-12 px-4">
     {connectedAccount ? (<h3 className="text-white text-3xl text-center my-2"> Latest Transactions </h3>): (<h3 className="text-white text-3xl text-center my-2"> Connect your Account </h3>)}
 
-  </div>
+<div className="flex flex-wrap justify-center items-center mt-10">
+  {
+    dummyData.reverse().map((transactions, i)=>(<TransactionCard key={i} {...transactions} />))
+  }
 
+</div>
+  </div>
 </div>
   )
 }
